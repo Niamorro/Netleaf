1e1b24eca0a07a23f3962dfefe12ce791f22a267
Niamorro <123011549+Niamorro@users.noreply.github.com>
2023-12-10 15:23:33 +0300

    Delete Netleaf Project directory

--- Netleaf Project/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from gui import DeviceScanner
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    scanner = DeviceScanner()
-    sys.exit(app.exec_())
